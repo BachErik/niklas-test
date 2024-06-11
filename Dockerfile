8d57359d1896f807d65a7776fbcd94ec25449813
Niklas S <nikoverflow007@gmail.com>
2024-06-11 18:17:40 +0200

    :)

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ WORKDIR /tools
 COPY replacer.py ./
 RUN apt update
 RUN apt install wget -y
+RUN wget https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/$(dpkg --print-architecture)/kubectl
+RUN chmod +x kubectl
 RUN ARCH=$(dpkg --print-architecture) && wget https://get.helm.sh/helm-v3.10.3-linux-$ARCH.tar.gz && tar xf helm-v3.10.3-linux-$ARCH.tar.gz && mv linux-$ARCH/helm ./helm && rm -rf linux-$ARCH helm-v3.10.3-linux-$ARCH.tar.gz
 RUN chmod +x helm
 RUN pip install kubernetes
--- replacer.py
+++ replacer.py
@@ -4,13 +4,10 @@ from kubernetes import client, config
 def read_data(namespace, type, name, key):
     if type == "secret":
         secret = api_instance.read_namespaced_secret(name, namespace)
-        if secret.data:
-            return base64.b64decode(secret.data.get(key)).decode()
-    if type == "configmap":
+        return base64.b64decode(secret.data.get(key)).decode()
+    else:
         config_map = api_instance.read_namespaced_config_map(name, namespace)
-        if config_map.data:
-            return config_map.data.get(key)
-    return ""
+        return config_map.data.get(key)
 
 def replace_pattern(match):
     namespace, type, name, key = match.groups()
@@ -19,9 +16,8 @@ def replace_pattern(match):
 
 if __name__ == "__main__":
     input = sys.stdin.read()
-    #config.load_kube_config()
     config.load_incluster_config()
     api_instance = client.CoreV1Api()
-    pattern = r'<([^:]+):([^:]+):([^:]+):([^>]+)>'
+    pattern = r'<([a-z0-9](?:[-a-z0-9]*[a-z0-9])?):(secret|configmap):([a-z0-9](?:[-a-z0-9]*[a-z0-9])?(?:\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*):([-._a-zA-Z0-9]+)>'
     output = re.sub(pattern, replace_pattern, input)
     print(output, end="")
