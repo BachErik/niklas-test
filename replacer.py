71d988f074506e97a79e85f048a0755b4e9643d6
Niklas S <nikoverflow007@gmail.com>
2024-06-15 15:31:40 +0200

    Update replacer.py

--- replacer.py
+++ replacer.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     input = sys.stdin.read()
     config.load_incluster_config()
     api_instance = client.CoreV1Api()
-    pattern = r'<([a-z0-9](?:[-a-z0-9]*[a-z0-9])?):(secret|configmap):([a-z0-9](?:[-a-z0-9]*[a-z0-9])?(?:\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*):([-._a-zA-Z0-9]+)>'
+    pattern = r'<([a-z0-9](?:[-a-z0-9]*[a-z0-9])?):(secret|configmap):([a-z0-9](?:[-a-z0-9]*[a-z0-9])?(?:\.[a-z0-9](?:[-a-z0-9]*[a-z0-9])?)*):([-._a-zA-Z0-9]+)>'
     output = re.sub(pattern, replace_pattern, input)
     print(output, end="")
